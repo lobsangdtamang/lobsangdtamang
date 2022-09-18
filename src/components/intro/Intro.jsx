e7dd3847d42d8eb6b7d318dd366e97e33a118782
lobsangdtamang <lobsangdtamang@gmail.com>
2022-09-18 23:17:22 +1000

    small changes

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -24,7 +24,6 @@ export default function Intro() {
         <div className="right">
             <div className="wrapper">
               <h2>Hi There, I'm</h2>
-              <h1>Lobsang</h1>
               <h3>Freelance <span ref={textRef}></span></h3>
             </div>
             <a href="#portfolio">
